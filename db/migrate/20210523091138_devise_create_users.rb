0060a869ce2d10dfd6e8ed9db0672f6d7a7a686b
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-05-24 11:51:55 +0900

    usersテーブルにカラム追加（マイグレーションファイル修正）

--- db/migrate/20210523091138_devise_create_users.rb
+++ db/migrate/20210523091138_devise_create_users.rb
@@ -4,8 +4,17 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :nickname,           null: false, default: ""
+      t.string :email,              null: false, default: "", unique: true
       t.string :encrypted_password, null: false, default: ""
+      t.string :last_name,          null: false, default: ""
+      t.string :first_name,         null: false, default: ""
+      t.string :last_name_kana,     null: false, default: ""
+      t.string :first_name_kana,    null: false, default: ""
+      t.date :birth_day,            null: false
+      
+
+
 
       ## Recoverable
       t.string   :reset_password_token
@@ -19,7 +28,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.datetime :current_sign_in_at
       # t.datetime :last_sign_in_at
       # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+      # t.string   :last_sign_in_ip 
 
       ## Confirmable
       # t.string   :confirmation_token
