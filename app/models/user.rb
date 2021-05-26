3cc5764b2d301dee6907d2f328ca89e478eba6dc
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-05-26 11:06:26 +0900

    userモデルにバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_day, presence: true
 end
