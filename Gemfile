fb5d4e5c1fa416f38b99c593921bd2d158de9ca2
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-05-27 14:12:45 +0900

    デバックツール（pry-rails）導入

--- Gemfile
+++ Gemfile
@@ -59,3 +59,4 @@ group :development do
 end
 
 gem 'devise'
+gem 'pry-rails'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "ユーザー新規登録" do
+    it "nicknameが空だと登録できない" do
+      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      binding.pry
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it "emailが空では登録できない" do
+    end
+  end
 end
