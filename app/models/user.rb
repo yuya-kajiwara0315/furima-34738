58d620820cd34970b809c6a3e4e9f14466b07d86
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-05-28 11:47:00 +0900

    ターミナルで警告が出ないよう修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, uniqueness: true
+  validates :email, uniqueness: { case_sensitive: true }
   validates :password, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
