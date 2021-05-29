694965bc756196f06d36c6c534de86015daec0a6
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-05-29 17:51:40 +0900

    不要なコメントアウトを削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -11,24 +11,3 @@ FactoryBot.define do
     birth_day             { "2000-01-01" }
   end
 end
-
-# FactoryBot.define do
-#   factory :user do
-#     nickname              {"toda"}
-#     email                 {"totodada@gmail.com"}
-#     password              {"12345678"}
-#     password_confirmation {"12345678"}
-#   end
-# end
-
-
-
-# nickname              {Faker::Name.name}
-# email                 {Faker::Internet.free_email}
-# password              {Faker::Internet.password(min_length: 6)}
-# password_confirmation {password}
-# first_name            {person.first.kanji}
-# last_name             {person.last.kanji}
-# first_name_kana       {person.first.katakana}
-# last_name_kana        {person.last.katakana}
-# birth_day             {}
