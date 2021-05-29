69899c5d167390279163e4073442911724f4501e
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-05-29 16:23:07 +0900

    rubocopを使って修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'yamada太郎'}
-    email                 {'yamada@gmail.com'}
-    password              {'yamada00'}
-    password_confirmation {password}
-    first_name            {'山田'}
-    last_name             {'太郎'}
-    first_name_kana       {'ヤマダ'}
-    last_name_kana        {'タロウ'}
-    birth_day             {"2000-01-01"}
+    nickname              { 'yamada太郎' }
+    email                 { Faker::Internet.free_email }
+    password              { 'yamada00' }
+    password_confirmation { password }
+    first_name            { '山田' }
+    last_name             { '太郎' }
+    first_name_kana       { 'ヤマダ' }
+    last_name_kana        { 'タロウ' }
+    birth_day             { "2000-01-01" }
   end
 end
 
