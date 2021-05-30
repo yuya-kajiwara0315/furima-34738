21c40b58b78e6173f02c75b1440ed25c1b9041bc
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-05-30 10:52:30 +0900

    ルーティングの不要なコードを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-
   devise_for :users
-  get 'items/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "items#index"
-  resources :users, only: [:index, :new, :create]
 end
