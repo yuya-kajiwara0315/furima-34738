25793a460e7f04dc09bbf3dba830e198c2c088d4
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-01 12:00:14 +0900

    ターミナルでコマンドを間違えた（rails g model item）ため削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_013956) do
-
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", default: "", null: false
-    t.string "description", default: "", null: false
-    t.integer "category_id", null: false
-    t.integer "condition_id", null: false
-    t.integer "charge_id", null: false
-    t.integer "prefecture_id", null: false
-    t.integer "date_id", null: false
-    t.integer "selling_price", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_items_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2021_05_23_091138) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
@@ -45,5 +30,4 @@ ActiveRecord::Schema.define(version: 2021_06_01_013956) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "items", "users"
 end
