8701078db96ab45badb56f772efbc3c744c8824f
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-03 10:29:34 +0900

    未ログイン時に「出品する」ボタンを押すとログイン画面に遷移する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,11 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:new]
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   private
@@ -10,5 +13,9 @@ class ItemsController < ApplicationController
   # ActiveStorage未実装
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :charge_id, :prefecture_id, :date_id, :selling_price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
