6f42bd394f88224155733444462a704168991e6c
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-03 10:37:26 +0900

    createアクションとストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,4 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:new]
   def index
     @items = Item.all
   end
@@ -8,14 +7,19 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
   # ActiveStorage未実装
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :charge_id, :prefecture_id, :date_id, :selling_price).merge(user_id: current_user.id)
-  end
-
-  def set_item
-    @item = Item.find(params[:id])
   end
 end
