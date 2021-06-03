4cc07d84797b044f945982bae89d4c2cafb1f868
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-03 10:19:37 +0900

    ストロングパラメータ追加（permit）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,6 @@ class ItemsController < ApplicationController
 
   # ActiveStorage未実装
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :charge_id, :prefecture_id, :date_id, :selling_price).merge(user_id: current_user.id)
   end
 end
