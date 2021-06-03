9b5a3542cc6ffbcb2933f10ea157e97667c3287b
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-03 12:42:02 +0900

    itemモデルにバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :date_id, presence: true
+  validates :selling_price, presence: true
+  
+  
   has_one_attached :image
 end
