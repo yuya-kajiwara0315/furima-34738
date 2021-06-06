49f57a02cc2f2f30a22280ef1f3a17e126727619
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-06 11:45:14 +0900

    外部キーを使っているモデルをテスト（user_id）

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,15 +3,13 @@ FactoryBot.define do
   factory :item do
     name           { '商品テスト' }
     description    { '商品テストです。' }
-    category_id    { '2' }
-    condition_id   { '2' }
-    charge_id      { '2' }
-    prefecture_id  { '2' }
-    delivery_id    { '2' }
-    selling_price  { '300' }
-    user_id        { '2' }
-
-   
+    category_id    { 2 }
+    condition_id   { 2 }
+    charge_id      { 2 }
+    prefecture_id  { 2 }
+    delivery_id    { 2 }
+    selling_price  { 300 }
+    user_id        { 2 }
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
