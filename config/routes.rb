78d1426dfa07974563922f2ca62f864dd859c251
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-12 08:39:59 +0900

    updateアクションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :show, :edit]
+  resources :items, only: [:index, :new, :create, :show, :edit, :update]
 end
