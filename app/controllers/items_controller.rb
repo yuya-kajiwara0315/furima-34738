9988a5691d981c73f82299594dea6005ae9e4bb2
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-12 20:00:10 +0900

    ログインページに遷移するように修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,7 +47,7 @@ class ItemsController < ApplicationController
 
   def move_to_index
     unless user_signed_in?
-      redirect_to action: :index
+      redirect_to action: :new
     end
   end
 end
