6bbd73a56006d1e5fcb07c1eb1ebfa9506c67e21
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-12 15:55:02 +0900

    商品情報編集後の条件分岐（redirect_to、render）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,8 +27,12 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
 
