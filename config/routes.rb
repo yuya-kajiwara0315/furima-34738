d1f24de54c56b73d590dbcd027bb178b9a490fbb
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-14 09:57:27 +0900

    ７つのアクションが揃ったのでonly削除

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :items
 end
