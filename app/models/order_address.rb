26c1f2e45a215a3c8b70035885da7de834957f7a
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-18 11:35:20 +0900

    バリデーションを設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,4 +1,17 @@
 class OrderAddress
   include ActiveModel::Model
   attr_accessor :address, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :address,       format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city,          format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください'}
+    validates :house_number,  format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください'}
+    validates :phone_number,  format: {with: /\A0[0-9]{9,10}\z/}
+
+    validates :user_id
+    validates :item_id
+  end
+
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+
 end
