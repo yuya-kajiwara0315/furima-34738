c08f562c5aebc6b21522686ac4d9ad0ff94e8023
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-19 08:37:11 +0900

    ストロングパラメータにて受け取るパラメーターに制限をかける

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,14 @@ class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
   end
+
+  def create
+    @order_address = OrderAddress(order_params)
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:address, :prefecture_id, :city, house_number, building_name, phone_number).merge(user_id: current_user.id)
+  end
 end
