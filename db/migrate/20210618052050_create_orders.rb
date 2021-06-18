b7cb9cb1c868957f92059ccc893183b60ec618c1
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-18 16:06:25 +0900

    ２つのマイグレーションファイル（order, address）編集

--- db/migrate/20210618052050_create_orders.rb
+++ db/migrate/20210618052050_create_orders.rb
@@ -1,6 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
+      t.references :item, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
