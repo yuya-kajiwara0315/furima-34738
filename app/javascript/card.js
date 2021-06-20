e396f5ebb009815a3893e7d79f8556140fb6a040
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-20 14:45:49 +0900

    PAY.JPテスト公開鍵の情報は公開できない

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,10 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_******************");
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+    
+
   });
 };
 
