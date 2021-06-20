236a14393fea15b38ff9e485969bb9e64f4fa5be
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-20 15:43:48 +0900

    クレジットカード情報を送信してトークンが生成されるか確認（一時的に仮置きを解除）

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -14,6 +14,13 @@ const pay = () => {
       exp_year: `20${formData.get("order_address[exp_year]")}`,
       cvc: formData.get("order_address[cvc]"),
     };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
   });
 };
 
