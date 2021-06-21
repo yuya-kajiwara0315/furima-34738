4e2d2c09f41b9453ce118e541224d093a1d75964
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-21 09:18:53 +0900

    フォームに存在するクレジットカードの各情報を削除

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -23,7 +23,10 @@ const pay = () => {
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
 
-
+      document.getElementById("card_number").removeAttribute("name");
+      document.getElementById("card_exp_month").removeAttribute("name");
+      document.getElementById("card_exp_year").removeAttribute("name");
+      document.getElementById("card_cvc").removeAttribute("name");
     });
   });
 };
