89c7d529ae674196b4cb7a55ae8c0125ef77aba6
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-25 12:19:06 +0900

    ストロングパラメーターを追加（item cannot be blank）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,7 +18,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:address, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, token: params[:token])
+    params.require(:order_address).permit(:address, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, token: params[:token], item_id: @item.id)
   end
 
   def move_to_index_create
