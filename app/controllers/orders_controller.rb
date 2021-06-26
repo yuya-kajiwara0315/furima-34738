55f2b69244c84b2af766c9703b766c6fa7054a83
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-26 15:36:46 +0900

    エラー修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -39,6 +39,6 @@ class OrdersController < ApplicationController
   end
 
   def sold_out_item
-    redirect_to root_path if @item.purchase.present?
+    redirect_to root_path if @item.item.present?
    end
 end
