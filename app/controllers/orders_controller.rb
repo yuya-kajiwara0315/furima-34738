41a26f314fab477a798101efada62f73cdbcc845
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-27 14:00:00 +0900

    ログアウト状態ではURLを直接入力したらログインページに遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :move_to_index_create, only: [:index, :create]
   before_action :sold_out_item, only: [:index]
+  before_action :authenticate_user!, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
