a4b9889c01c16eeb31f76209b03fff95b454edef
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-28 14:10:38 +0900

    tokenにもpresence: trueを適用

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -8,6 +8,8 @@ class OrderAddress
     validates :house_number
     validates :phone_number,  format: {with: /\A[0-9]{10,11}\z/}
 
+    validates :token
+
     validates :user_id
     validates :item_id
   end
