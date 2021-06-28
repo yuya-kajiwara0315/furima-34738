6ebb337430a90a7d5db21bacda79b449fff00290
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-28 14:05:23 +0900

    tokenの情報を書く場所を変更

--- spec/factories/order_address.rb
+++ spec/factories/order_address.rb
@@ -6,5 +6,6 @@ FactoryBot.define do
     house_number { '１ー１' }
     building_name { '東京ハイツ' }
     phone_number { '09012345678' }
+    token {"tok_abcdefghijk00000000000000000"}
   end
 end
--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -2,6 +2,5 @@ FactoryBot.define do
   factory :order do
     item_id {1}
     user_id {1}
-    token {"tok_abcdefghijk00000000000000000"}
   end
 end
