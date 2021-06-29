f618014bf5c2cc3a09dfde4f3976fe31c0e267ad
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-06-29 10:03:46 +0900

    テストコード追加（token, user_id, item_id）

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -90,6 +90,25 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Phone number is invalid")
       end
+
+      it 'tokenが空では登録できないこと' do 
+        @order_address.token = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
+
+      it 'user_idが空では登録できないこと' do 
+        @order_address.user_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("User can't be blank")
+      end
+
+      it 'item_idが空では登録できないこと' do 
+        @order_address.item_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
+      end
+
     end
 
   end
