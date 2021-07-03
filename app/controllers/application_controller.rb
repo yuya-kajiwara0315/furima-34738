2c4a29dee097387516d9dc07b1ef32cf00175962
yuya-kajiwara0315 <tamanigame@gmail.com>
2021-07-03 17:44:29 +0900

    環境変数の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :basic_auth
+  before_action :basic_auth
 
 
 
@@ -12,9 +12,9 @@ class ApplicationController < ActionController::Base
 
   private
 
-  # def basic_auth
-  #   authenticate_or_request_with_http_basic do |username, password|
-  #     username == 'admin' && password == '2222'
-  #   end
-  # end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
